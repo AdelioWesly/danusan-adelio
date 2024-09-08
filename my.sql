fa8eebdf3dfe893b4540d5b9e740e208cfb385df
adeliowesley <adeliowerfetes@gmail.com>
2024-09-08 23:26:43 +0700

    Move public files to root for GitHub Pages

--- my.sql
+++ my.sql
@@ -1,6 +1,10 @@
-CREATE TABLE produk3(
+CREATE TABLE produk6(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(255) NOT NULL,
 	comment TEXT NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+INSERT INTO produk6 (username, comment, created_at) 
+VALUES ('john_doe', 'Great post!', NOW());
